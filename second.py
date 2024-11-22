c5ddef723fc55efd95a97a984f4dfe671235a8b0
Fatummm <lumik2006@gmail.com>
2024-11-22 12:37:58 +0300

    changed second.py

--- second.py
+++ second.py
@@ -2,5 +2,6 @@ import time
 import random
 
 if __name__ == "__main__":
-    lst = [random.randint(0, 10) for _ in range(10)]
+    random.seed(time.time())
+    lst = [random.randint(0, 20) for _ in range(10)]
     print(*lst) # 1 1 8 1 0 3 8 2 9 4
